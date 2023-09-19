4c0964944a679f8dbf899a0f426bb7f040ddd2b1
TrentSalas01 <112633819+TrentSalas01@users.noreply.github.com>
2023-09-19 16:06:23 -0400

    login/register beta

--- app.py
+++ app.py
@@ -33,14 +33,57 @@ def hello_world():
     return render_template("home.html")
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        registername = str(request.form.get("username"))
+        print(registername)
+        registerpassword = str(request.form.get("password"))
+        registerpassword = generate_password_hash(registerpassword)
+        registeremail = str(request.form.get("email"))
+        conn = connectDB()
+        id = int(str(conn.execute("SELECT COUNT(*) FROM User").fetchall()[0]).strip('(').strip(')').strip(','))+1
+        conn.execute("INSERT INTO User VALUES (0, ?, ?, ?, ?, 0)", (registeremail, id, registerpassword, registername))
+        conn.commit()
+        return "SUCCESS"
+    else:
+        return render_template("login.html")
+        
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == "POST":
+        print("dwdwdw")
         loginname = str(request.form.get("username"))
         loginpassword = str(request.form.get("password"))
         
         conn = connectDB()
+        user = str(conn.execute("SELECT Name FROM User WHERE Name=?", (loginname,)).fetchall()).strip('(').strip(')').strip(',')
+        password = str(conn.execute("SELECT Password FROM User WHERE Name=?", (loginname,)).fetchall()).strip('(').strip(')').strip(',')
         
-    return render_template("login.html")
         
+        password = list(password)
+        newp = ""
+        for i in password:
+            if i.isalnum() or i in ['!', '%', '$', '#', ':']:
+                newp += i
         
+        user= list(user)
+        newu = ""
+        for i in user:
+            if i.isalnum() or i in ['!', '%', '$', '#']:
+                newu += i
+        
+        if newu==loginname and check_password_hash(newp, loginpassword)==True:
+            return "SUCCESS"
+        else:
+            return "User Not Found"
+    else:
+        return render_template("login.html")
+    
+def connectDB():
+    conn = None
+    try:
+        conn = sqlite3.connect('TheBank.db')
+    except sqlite3.Error as e:
+        print(e)
+    return conn
